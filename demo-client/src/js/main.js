fe772a251abac55b64e1ed2c40689828e5a89e1d
Esteban Hernandez <hernandez.dev.services@gmail.com>
2020-10-11 16:47:21 -0400

    Request to demo service include correlation ID

--- demo-client/src/js/main.js
+++ demo-client/src/js/main.js
@@ -1,7 +1,26 @@
+import 'whatwg-fetch'
+import fetchIntercept from 'fetch-intercept';
+import { v4 as correlationId } from 'uuid';
+
+const API_URL = 'http://localhost:8080';
+
 document.addEventListener('DOMContentLoaded', function () {
+
+    fetchIntercept.register({
+        request: (url, config) => {
+            if (url.includes(API_URL)) {
+                return [
+                    `${url}${url.includes('?') ? '&' : '?'}cid=${correlationId()}`,
+                    config
+                ];
+            }
+            return [url, config];
+        }
+    });
+
     document.querySelectorAll('div').forEach(div => {
         div.addEventListener('click', () => {
-            fetch(`http://localhost:8080/${div.id}`)
+            fetch(`${API_URL}/${div.id}`)
                 .then(response => response.text())
                 .then(message => console.log(message))
                 .catch(error => console.error(error));
