1c770c8cac73a43ee2fc623d75fdf3a16113b585
Esteban Hernandez <hernandez.dev.services@gmail.com>
2020-10-11 15:44:44 -0400

    Added Demo service

---
+++ demo-service/src/main/java/com/example/correlationiddemo/CorrelationIdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.correlationiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrelationIdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrelationIdDemoApplication.class, args);
+	}
+
+}
