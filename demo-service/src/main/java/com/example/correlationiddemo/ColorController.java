b7445ff5f5bf9f1ffddfe76aada78c3cdb8a31f9
Esteban Hernandez <hernandez.dev.services@gmail.com>
2020-10-11 16:50:30 -0400

    Added example log statements

--- demo-service/src/main/java/com/example/correlationiddemo/ColorController.java
+++ demo-service/src/main/java/com/example/correlationiddemo/ColorController.java
@@ -1,28 +1,37 @@
 package com.example.correlationiddemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ColorController {
 
+    private static Logger logger = LoggerFactory.getLogger(ColorController.class);
+
     @GetMapping("/blue")
     public String blue() {
+        logger.info("Request received by blue.");
         return "Blue is the warmest color.";
     }
 
     @GetMapping("/red")
     public String red() {
+        logger.info("Request received by red.");
         return "Red is my favorite color.";
     }
 
     @GetMapping("/purple")
     public String purple() {
+        logger.info("Request received by purple.");
         return "Purple is the color of royalty.";
     }
 
     @GetMapping("/green")
     public String green() {
+        logger.info("Request received by green.");
         return "Green is the color of money.";
     }
 
